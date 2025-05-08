bcdbc960838c74bba28da3e8d7b120978d99ff55
ririgly.4@gmail.com <Elisabeth0321>
2025-05-08 13:35:36 +0300

    fix FileManager.java with plugin support

--- src/main/java/com/lizaveta/utils/FileManager.java
+++ src/main/java/com/lizaveta/utils/FileManager.java
@@ -30,13 +30,27 @@ public class FileManager {
         int result = fileChooser.showOpenDialog(parent);
         if (result == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
-            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file))) {
+
+            try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file)) {
+                @Override
+                protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+                    try {
+                        ClassLoader pluginCL = PluginLoader.getPluginClassLoader();
+                        if (pluginCL != null) {
+                            return Class.forName(desc.getName(), false, pluginCL);
+                        }
+                    } catch (ClassNotFoundException e) {
+                    }
+                    return super.resolveClass(desc);
+                }
+            }) {
                 ShapeList loadedShapes = (ShapeList) ois.readObject();
                 shapeList.getShapes().clear();
                 shapeList.getShapes().addAll(loadedShapes.getShapes());
                 drawingPanel.repaint();
                 controlPanel.updateButtonsState();
             } catch (IOException | ClassNotFoundException e) {
+                e.printStackTrace();
                 JOptionPane.showMessageDialog(parent, "Ошибка при открытии файла!", "Ошибка", JOptionPane.ERROR_MESSAGE);
             }
         }
--- src/main/java/com/lizaveta/utils/PluginLoader.java
+++ src/main/java/com/lizaveta/utils/PluginLoader.java
@@ -7,14 +7,26 @@ import lombok.Getter;
 import java.io.File;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.HashMap;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 import javax.swing.*;
 
 public class PluginLoader {
     @Getter
     private final HashMap<String, ShapeCreator> shapeFactory = new HashMap<>();
 
+
+    private static final List<URL> pluginUrls = new ArrayList<>();
+    @Getter
+    private static URLClassLoader pluginClassLoader = new URLClassLoader(new URL[0], PluginLoader.class.getClassLoader());
+
+
     private final JFrame parent;
+
     private final ControlPanel controlPanel;
 
     public PluginLoader(JFrame parent, ControlPanel controlPanel) {
@@ -26,31 +38,31 @@ public class PluginLoader {
         JFileChooser fileChooser = new JFileChooser();
         fileChooser.setDialogTitle("Выберите JAR-плагин");
         fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("JAR файлы (*.jar)", "jar"));
-        fileChooser.setCurrentDirectory(new File("D:\\WORK\\4 СЕМЕСТР\\ООТПиСП"));
 
         int result = fileChooser.showOpenDialog(parent);
         if (result == JFileChooser.APPROVE_OPTION) {
             File file = fileChooser.getSelectedFile();
             try {
                 URL jarURL = file.toURI().toURL();
-                URLClassLoader classLoader = new URLClassLoader(new URL[]{jarURL}, getClass().getClassLoader());
+                pluginUrls.add(jarURL);
+                pluginClassLoader = new URLClassLoader(pluginUrls.toArray(new URL[0]), PluginLoader.class.getClassLoader());
 
-                java.util.jar.JarFile jarFile = new java.util.jar.JarFile(file);
-                java.util.Enumeration<java.util.jar.JarEntry> entries = jarFile.entries();
+                JarFile jarFile = new JarFile(file);
+                Enumeration<JarEntry> entries = jarFile.entries();
                 boolean found = false;
+
                 while (entries.hasMoreElements()) {
                     java.util.jar.JarEntry entry = entries.nextElement();
                     String name = entry.getName();
                     if (name.endsWith(".class")) {
                         String className = name.replace('/', '.').substring(0, name.length() - 6);
                         try {
-                            Class<?> clazz = classLoader.loadClass(className);
+                            Class<?> clazz = pluginClassLoader.loadClass(className);
                             if (ShapePlugin.class.isAssignableFrom(clazz)) {
                                 ShapePlugin plugin = (ShapePlugin) clazz.getDeclaredConstructor().newInstance();
                                 var shapes = plugin.getShapes();
                                 for (var entryShape : shapes.entrySet()) {
                                     String shapeName = entryShape.getKey();
-                                    String pluginName = file.getName().replace(".jar", "");
                                     ShapeCreator factory = entryShape.getValue();
                                     controlPanel.shapeFactory.put(shapeName, factory);
                                     controlPanel.shapeSelector.addItem(shapeName);
